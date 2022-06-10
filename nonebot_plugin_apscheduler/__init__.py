6bb159a91567bda85cada405c183abbb918fe010
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2022-06-10 03:48:01 +0000

    :sparkles: update black, isort, pre-commit, devcontainer

--- nonebot_plugin_apscheduler/__init__.py
+++ nonebot_plugin_apscheduler/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-from nonebot import get_driver, export
-from nonebot.log import logger, LoguruHandler
+from nonebot import export, get_driver
+from nonebot.log import LoguruHandler, logger
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from .config import Config
@@ -11,12 +11,12 @@ global_config = driver.config
 plugin_config = Config(**global_config.dict())
 
 scheduler = AsyncIOScheduler()
+scheduler.configure(plugin_config.apscheduler_config)
 export().scheduler = scheduler
 
 
 async def _start_scheduler():
     if not scheduler.running:
-        scheduler.configure(plugin_config.apscheduler_config)
         scheduler.start()
         logger.opt(colors=True).info("<y>Scheduler Started</y>")
 
--- nonebot_plugin_apscheduler/config.py
+++ nonebot_plugin_apscheduler/config.py
@@ -5,7 +5,8 @@ class Config(BaseSettings):
     apscheduler_autostart: bool = True
     apscheduler_log_level: int = 30
     apscheduler_config: dict = Field(
-        default_factory=lambda: {"apscheduler.timezone": "Asia/Shanghai"})
+        default_factory=lambda: {"apscheduler.timezone": "Asia/Shanghai"}
+    )
 
     class Config:
         extra = "ignore"
